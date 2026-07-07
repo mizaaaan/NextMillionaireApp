355959b22ed5ac101ebb6b09f0b3fceab75a4b88
Md Mizanur Rahman <30289535+mizaaaan@users.noreply.github.com>
2026-07-07 06:20:31 +0600

    Enable inline media playback and adjust playback settings

--- App.js
+++ App.js
@@ -103,6 +103,8 @@ export default function App() {
             sharedCookiesEnabled
             domStorageEnabled
             javaScriptEnabled
+            allowsInlineMediaPlayback
+            mediaPlaybackRequiresUserAction={false}
           />
         )}
 
